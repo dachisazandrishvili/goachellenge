9244716c40feca1b14b38af4831cbac16b009e80
dachisazandrishvili <146382038+dachisazandrishvili@users.noreply.github.com>
2023-12-30 22:51:39 +0400

    Add files via upload

---
+++ classwork/python.py
@@ -0,0 +1,15 @@
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+
+math = input("chose one: / , * ,+ , -: ")
+
+if math =="/":
+  print(num1 / num2)
+elif math == "*":
+  print(num1 * num2)
+elif math == "+":
+  print(num1 + num2)
+elif math == "-":
+  print(num1 - num2)
+else:
+  print("sorry we can't slove your problem")
